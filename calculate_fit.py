4228e27c6fc370f1a54ffc8dab38fa197b215f05
fstihler <frederik.stihler@berkeley.edu>
2023-12-01 15:50:23 -0800

    small updates

---
+++ calculate_fit.py
@@ -0,0 +1,17 @@
+""" 
+Module to calculate the fitness of the current generation
+"""
+
+import numpy as np
+import pandas as pd
+
+class calculate_fit:
+    
+    def __init__():
+        pass
+
+    def xxx():
+        
+        
+        
+    def select_features(self, current_population):
--- mutate.py
+++ mutate.py
@@ -1,6 +1,7 @@
 """ 
 Mutation module with genetic operator "mutation"
 """
+
 import numpy as np
 
 class mutation:
@@ -8,7 +9,7 @@ class mutation:
     def __init__():
         pass
 
-    def random_mutate():
+    def random_mutate(self, current_population):
         
         """
         Randomly switches genes (bit switch) in generation with probability mutate_prob
@@ -21,7 +22,7 @@ class mutation:
     # initialize random generator
     rng = np.random.default_rng()
     
-    population_new = self.current_population.copy()
+    population_new = current_population.copy()
     mutation_locations = rng.binomial(1, self.mutate_prob, size = current_population.shape)
     mask = mutation_locations == 1
     population_new[mask] = 1 - population_new[mask] # flip bits using the mask
