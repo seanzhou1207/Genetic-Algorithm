2bd24684bcc05e110762725a9024ce7eb015084e
Xiaoyu (Sean) Zhou <seanzhou1207@gmail.com>
2023-12-05 18:10:58 -0800

    First draft of the algo

--- gavs.py
+++ gavs.py
@@ -5,10 +5,14 @@ from typing import Union
 
 from utils import calculate_fit
 from utils import parent_select
+from utils import cross_over
+from utils import mutate
 
-class GA (calculate_fit.calculate_fit,
-          parent_select.ParentSelection
-        ):
+class GA (calculate_fit.CalculateFit,
+          parent_select.ParentSelection,
+          cross_over.CrossOver,
+          mutate.Mutation
+         ):
     supported_int_types = Union[int, np.int8, np.int16, np.int32, np.int64,
                            np.uint, np.uint8, np.uint16, np.uint32, np.uint64]
     supported_float_types = Union[float, np.float16, np.float32, np.float64]
@@ -18,7 +22,7 @@ class GA (calculate_fit.calculate_fit,
                 mod,
                 max_iter: int,
                 pop_size: int = None, 
-                fitness_func = "AIC",
+                #fitness_func = "AIC",
                 starting_population = None,
                 mutate_prob = 0.01,
                 save_sols = False,
@@ -43,7 +47,7 @@ class GA (calculate_fit.calculate_fit,
         self.mod = mod
         self.max_iter = max_iter
         self.mutate_prob = mutate_prob
-        self.fitness_func = fitness_func
+        #self.fitness_func = fitness_func
         self.starting_population = starting_population
         self.current_population = None
 
@@ -53,26 +57,37 @@ class GA (calculate_fit.calculate_fit,
             pass
 
     def initialize_pop(self):
-        if not self.starting_population:    # Specify a starting pop
+        if not isinstance(self.starting_population, np.ndarray):    # Specify a starting pop
             rows = self.pop_size
+            if rows % 2 == 1:    # If pop_size is odd
+                self.pop_size = self.pop_size + 1    # Only allow even number for population size
+            
             cols = self.C
             self.starting_population = np.random.choice([0, 1], size=(rows, cols))    # Complete random generation
             
             return self.starting_population
         else:
             return self.starting_population
- 
-    def calculate_fit(self):
-        pass
+     
+    def select(self, operator_list):
+        starting_pop = self.initialize_pop()
+        current_pop = starting_pop.copy()
 
-    def choose_parents(self):
-        pass
+        for i in range(self.max_iter):
+            chrom_ranked, fitness_val = self.calc_fit_sort_population(current_pop)
+            
+            parents = self.select_from_fitness_rank(chrom_ranked)
+            current_pop = parents
+            for method in operator_list:
+                new_population = method(current_pop)
+                current_pop = new_population
+            #old_pop = new_offspring.copy()
+        
+        final_pop = current_pop.copy()
+        final_pop_sorted, final_fitness_val = self.calc_fit_sort_population(final_pop)
+        
+        return (final_pop_sorted, final_fitness_val)
 
-    def cross_over(self):
-        pass
 
-    def mutate(self):
-        pass
-    
-    def select(self):
-        pass
+            
+
