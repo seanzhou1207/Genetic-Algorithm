4564ef80f0921c5422d9832ad41a7617935a8ab0
Xiaoyu (Sean) Zhou <seanzhou1207@gmail.com>
2023-12-10 22:36:08 -0800

    improved structure for init/cal_fit

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from .calculate_fit import _CalculateFit
+from .cross_over import _CrossOver
+from .mutate import _Mutation
+from .parent_select import _ParentSelection
--- utils/calculate_fit.py
+++ utils/calculate_fit.py
@@ -1,17 +1,18 @@
 """ 
 Module to calculate the fitness of the current generation
 """
-
+from typing import List, Tuple
+from numpy import ndarray
 import numpy as np
-import pandas as pd
-import statsmodels.api as sm
 
-class CalculateFit:
+class _CalculateFit:
     
-    def __init__():
+    def __init__(self):
         pass
     
-    def calc_fit_sort_population(self, current_population):
+    def calc_fit_sort_population(self, 
+        current_population: ndarray
+    ) -> Tuple[ndarray, ndarray]:
         """
         Calculated fitness of organisms and sorts population based on fitness score (AIC). From low AIC (best) to high.
         
@@ -19,10 +20,12 @@ class CalculateFit:
         Outputs: Sorted population, sorted fitness scores
         """
         
-        fitness_scores = self.calculate_fit_of_population(current_population)
+        fitness_scores: ndarray = self.calculate_fit_of_population(current_population)
         return self.sort_population(current_population, fitness_scores)
     
-    def sort_population(self, current_population, fitness_scores):
+    def sort_population(self, 
+        current_population: ndarray, fitness_scores
+        )-> Tuple[ndarray, ndarray]:
         """
         Sorts population based on fitness score (AIC). From low AIC (best) to high.
         
@@ -35,7 +38,9 @@ class CalculateFit:
         return current_population[sort_index], fitness_scores[sort_index]
     
     
-    def calculate_fit_of_population(self, current_population):
+    def calculate_fit_of_population(self, 
+        current_population: ndarray
+        ) -> ndarray:
         """
         Calculates fitness of all organism in generation.
         
@@ -48,7 +53,7 @@ class CalculateFit:
             fitness_scores.append(self.calculate_fit_per_organism(X_trimmed))
         return np.array(fitness_scores)
             
-    def calculate_fit_per_organism(self, X_trimmed):
+    def calculate_fit_per_organism(self, X_trimmed: ndarray) -> float:
         """
         Calculates fitness of one organism based on trimmed data according to its allels.
         
@@ -58,7 +63,7 @@ class CalculateFit:
         mod_fitted = self.mod(self.y, X_trimmed).fit() 
         return mod_fitted.aic
         
-    def select_features(self, organism):
+    def select_features(self, organism: ndarray) -> ndarray:
         """
         Drops non-relevant features from data based on allels of an organism.
         
@@ -66,5 +71,7 @@ class CalculateFit:
         Outputs: Data to be used for fitness calculation of this organism
         """
       
-        X_trimmed = self.X.drop(columns=self.X.columns[organism == 0], axis=1)
+        #X_trimmed = self.X.drop(columns=self.X.columns[organism == 0], axis=1)
+        X_trimmed = self.X[:, organism != 0]
+
         return X_trimmed
